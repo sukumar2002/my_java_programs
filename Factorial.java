11496bdebd2905a2fd55a06123e5b358ddf9985e
sukumar <sukumarsavarapu7@gmail.com>
2025-02-01 15:59:44 +0530

    my seventh github program

---
+++ Factorial.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Factorial
+{
+    public static int findFact(int num)
+    {
+        int sum=1;
+         for(int i=num;i>=1;i--)
+         {
+
+             sum = sum*i;
+         }
+         return sum;
+
+    }
+    public static void main(String[] args)
+    {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number:");
+            int num = sc.nextInt();
+            System.out.println("Factorial is:" + findFact(num));
+        }
+        catch(Exception e)
+        {
+
+            System.out.print("Please enter numeric numbers only");
+        }
+    }
+}
