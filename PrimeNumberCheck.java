14fcb02f2069041dc613c6ba1108d3e3fecfc5e6
sukumar <sukumarsavarapu7@gmail.com>
2025-02-01 12:36:18 +0530

    my third github program

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeNumberCheck
+{
+    public static boolean isPrimeNumber(int num)
+    {
+         if(num==0||num==1)
+         {
+             System.out.println("Nor prime ");
+         }
+         if(num==2)
+         {
+             return true;
+         }
+         if(num%2==0)
+         {
+             return false;
+         }
+         return true;
+
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number to check prime or Not:");
+        int num=sc.nextInt();
+        if(num<0)
+        {
+            System.out.println("Negative numbers are not prime.");
+            return;
+        }
+        System.out.println("IsPrimeNumber:"+isPrimeNumber(num));
+    }
+}
