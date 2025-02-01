a8a25c66d1575e1a3d34afd138c704c4f6a95930
sukumar <sukumarsavarapu7@gmail.com>
2025-02-01 10:53:05 +0530

    my second github program

---
+++ ReverseString.java
@@ -0,0 +1,35 @@
+public class ReverseString {
+
+    public static String reverse(String str){
+
+            if (str == null || str.isBlank())
+            {
+                throw new IllegalArgumentException("String is empty");
+            }
+            //char[] arr=str.toCharArray();
+            StringBuilder reversed = new StringBuilder();
+            for (int i = str.length() - 1; i >= 0; i--) {
+                reversed.append(str.charAt(i));
+            }
+            return reversed.toString();
+
+
+
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        try
+        {
+           String str="q123 s daad s".trim();
+           System.out.println("Reverse String is:"+reverse(str));
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
