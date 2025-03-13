d367f77e56b2c0593961c090920502652c62cb54
sukumar <sukumarsavarapu7@gmail.com>
2025-03-13 14:51:10 +0530

    my 13 github program

---
+++ CountCharacterFrequency.java
@@ -0,0 +1,22 @@
+public class CountCharacterFrequency {
+    static void countFrequency(String s) {
+        int[] count = new int[256]; // Array for all ASCII characters
+
+        // Count occurrences of each character
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++;
+        }
+
+        // Print character frequencies
+        for (int i = 0; i < 256; i++) {
+            if (count[i] > 0) {
+                System.out.println((char) i + " : " + count[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "sukumar Savarapu @123!";
+        countFrequency(str);
+    }
+}
