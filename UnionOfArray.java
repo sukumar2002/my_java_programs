0b88a5df5d07d2cd0c9ad68d048ea3c17d56f230
sukumar <sukumarsavarapu7@gmail.com>
2025-03-17 22:08:57 +0530

    my 21 github program

---
+++ UnionOfArray.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class UnionOfArray {
+    static int[] union(int[] a1,int[] a2){
+        Set<Integer> set=new LinkedHashSet<>();
+        for(int n:a1){
+            set.add(n);
+        }
+
+        for(int nu:a2){
+              set.add(nu);
+            }
+       return set.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+    public static void main(String[] args) {
+        int[] arr1={3,58,101,4,83,2};
+        int[] arr2={33,11,9,3,101,4,83};
+        int[] res=union(arr1,arr2);
+        System.out.println(Arrays.toString(res));
+    }
+}
