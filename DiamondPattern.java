413dd75d3216c5188d5e188fab1747bf016cdee1
sukumar <sukumarsavarapu7@gmail.com>
2025-03-19 00:08:58 +0530

    my 23 github program

---
+++ DiamondPattern.java
@@ -0,0 +1,25 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
