a4cc4a5e749baf44f3c0c772a3e78824352f2568
sukumar <sukumarsavarapu7@gmail.com>
2025-04-04 21:38:10 +0530

    my 25 github program

---
+++ ToggleCase.java
@@ -0,0 +1,23 @@
+public class ToggleCase {
+    public static String toggleCase(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 65 && ch <= 90) {
+                result.append((char) ch + 32);
+            } else if (ch >= 97 && ch <= 122) {
+                result.append((char) (ch - 32));
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+        public static void main (String[]args){
+            String input = "hello sukumar!1234";
+            String toggled = ToggleCase.toggleCase(input);
+            System.out.println("Original: " + input);
+            System.out.println("Toggled: " + toggled);
+        }
+
+    }
