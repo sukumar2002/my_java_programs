aed255000641105283e56dbade295f66ff705c99
sukumar <sukumarsavarapu7@gmail.com>
2025-05-20 22:43:32 +0530

    my 42 github program

---
+++ FloydsTrianglePatteren.java
@@ -0,0 +1,17 @@
+public class FloydsTrianglePatteren {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=5;i++) {
+            int count=i;
+
+            for(int s=0;s>5;s++) {
+                System.out.print("  ");
+            }
+            for(j=1;j<=i;j++) {
+                System.out.print(count+" ");
+                count=count+j;
+            }
+            System.out.println();
+        }
+    }
+}
