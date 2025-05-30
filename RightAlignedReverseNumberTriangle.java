2f33eeeb18d4506d4e633c23788a616924ac611f
sukumar <sukumarsavarapu7@gmail.com>
2025-05-31 00:01:33 +0530

    my 45 github program

---
+++ RightAlignedReverseNumberTriangle.java
@@ -0,0 +1,14 @@
+public class RightAlignedReverseNumberTriangle {
+    public static void main(String[] args) {
+        for(int i=4;i>=1;i--){
+            for(int s=0;s<(4-i);s++){
+                System.out.print("  ");}
+                for(int j=i;j>=1;j--){
+                    System.out.print(j+" ");
+                }
+            System.out.println();
+            }
+
+        }
+    }
+
