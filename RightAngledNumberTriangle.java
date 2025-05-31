e5688fc7301c7942350df005912d1528c19e40a7
sukumar <sukumarsavarapu7@gmail.com>
2025-05-31 11:34:40 +0530

    my 45 github program

---
+++ RightAngledNumberTriangle.java
@@ -0,0 +1,11 @@
+public class RightAngledNumberTriangle {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=6;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
