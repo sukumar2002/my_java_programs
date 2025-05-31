9acdd1c4d61a215bbaa06b7fd2863af73cb0abfe
sukumar <sukumarsavarapu7@gmail.com>
2025-05-31 11:45:30 +0530

    my 47 github program

---
+++ AlternatingBinaryTrianglePatteren.java
@@ -0,0 +1,14 @@
+public class AlternatingBinaryTrianglePatteren {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2!=0){
+               System.out.print(0+" ");}
+                else{System.out.print(1+" ");}
+
+            }
+            System.out.println();
+        }
+    }
+
+}
