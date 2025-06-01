725d1a063534bd35cd72caa0942063e526a12411
sukumar <sukumarsavarapu7@gmail.com>
2025-06-01 23:43:43 +0530

    my 49 github program

---
+++ InvertedRightAlignedTriangle.java
@@ -0,0 +1,20 @@
+public class InvertedRightAlignedTriangle {
+    public static void main(String[] args) {
+        int n = 6; // Number of rows
+
+        System.out.println("Pattern 2: Inverted Right-Aligned Triangle\n");
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= n - i + 1; k++) {
+                System.out.print("*");
+            }
+            // Move to next line
+            System.out.println();
+        }
+    }
+}
