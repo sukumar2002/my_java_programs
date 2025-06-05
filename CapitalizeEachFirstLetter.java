0d446c8237e730bcebb7540bd4e54f05ab5343d3
sukumar <sukumarsavarapu7@gmail.com>
2025-06-05 23:01:41 +0530

    my 52 github program

---
+++ CapitalizeEachFirstLetter.java
@@ -0,0 +1,18 @@
+public class CapitalizeEachFirstLetter {
+    public static void main(String[] args) {
+        String str = "naresh it";
+        String[] words = str.trim().split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                result.append(Character.toUpperCase(word.charAt(0)))
+                        .append(word.substring(1).toLowerCase())
+                        .append(" ");
+            }
+        }
+
+        System.out.println(result.toString().trim());
+    }
+}
+
