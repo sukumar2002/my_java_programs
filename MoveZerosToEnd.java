39143cb22f3471bd0d7b095b0f5dbd3c68bfa5f8
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-06-20 22:19:29 +0530

    Update MoveZerosToEnd.java

--- MoveZerosToEnd.java
+++ MoveZerosToEnd.java
@@ -2,14 +2,32 @@ import java.util.Arrays;
 
 public class MoveZerosToEnd {
     public static int[] moveZerosToEnd(int[] arr){
-        int[] res=new int[arr.length];
-        int index=0;
-        for(int num:arr){
-            if(num!=0){
-                res[index++]=num;
-            }
-        }
-        return res;
+        // int[] res=new int[arr.length];
+        // int index=0;
+        // for(int num:arr){
+        //     if(num!=0){
+        //         res[index++]=num;
+        //     }
+        // }
+        // return res;
+        List<Integer> d = new LinkedList<>();
+
+		for (int num : arr) {
+			if (num != 0) {
+				d.add(num);
+			}
+		}
+		int s=arr.length-d.size();
+		for(int i=0;i<s;i++) {
+			d.add(0);
+		}
+
+		int[] result = new int[d.size()];
+		int index = 0;
+		for (int num : d) {
+			result[index++] = num;
+		}
+		return result;
     }
     public static void main(String[] args) {
         int[] arr={0,0,-1};
