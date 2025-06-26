87c4843404f2fb6b8490266829784396063f868b
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-06-26 23:12:06 +0530

    Update RemoveDuplicatesInString.java

--- RemoveDuplicatesInString.java
+++ RemoveDuplicatesInString.java
@@ -4,19 +4,20 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class RemoveDuplicatesInString {
-    static void remove(String str){
-          char[] c=str.toLowerCase().toCharArray();
-        Set<Character> s=new LinkedHashSet<>();
-       for(char st:c){
-           s.add(st);
-       }
-        for (char ch : s) {
-            System.out.print(ch);
-        }
-        System.out.println();
+    public static String removeDuplicates(String s) {
+		Map<Character, Integer> map = new LinkedHashMap<>();
+		for (char c : s.toCharArray()) {
+			map.put(c, map.getOrDefault(map, 0) + 1);
+		}
+
+		StringBuilder sb=new StringBuilder(map.size());
+		for(Map.Entry<Character, Integer> ss:map.entrySet()) {
+			sb.append(ss.getKey());
+		}
+		return sb.toString();
     }
     public static void main(String[] args) {
        String str="Communication";
-       RemoveDuplicatesInString.remove(str);
+      System.out.println("After removing duplicates : " + removeDuplicates(s));
     }
 }
